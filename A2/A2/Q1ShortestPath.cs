35db7d473e71e3aaca4e684a4558e66afd1474b1
ghzamani <ghzamaninejad@gmail.com>
2020-02-22 18:20:24 +0330

    A2 solved

---
+++ A2/A2/Q1ShortestPath.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A2
+{
+	public class Q1ShortestPath : Processor
+	{
+		public Q1ShortestPath(string testDataName) : base(testDataName) { }
+
+		public override string Process(string inStr) =>
+			TestTools.Process(inStr, (Func<long, long[][], long, long, long>)Solve);
+
+		public long Solve(long NodeCount, long[][] edges,
+						  long StartNode, long EndNode)
+		{
+			//saving graph in adjacency list
+			List<long>[] adjList = new List<long>[NodeCount];
+			for (int i = 0; i < NodeCount; i++)
+				adjList[i] = new List<long>();
+			for (int i = 0; i < edges.Length; i++)
+			{
+				adjList[edges[i][0] - 1].Add(edges[i][1]);
+				adjList[edges[i][1] - 1].Add(edges[i][0]);
+			}
+
+			long[] dist = new long[NodeCount];
+			for (long i = 0; i < NodeCount; i++)
+				dist[i] = long.MaxValue;
+			dist[StartNode - 1] = 0;
+
+			Queue<long> q = new Queue<long>();
+			q.Enqueue(StartNode);
+			while (q.Count != 0)
+			{
+				long u = q.Dequeue();
+				foreach(var v in adjList[u - 1])
+				{
+					if (dist[v - 1] == long.MaxValue)
+					{
+						q.Enqueue(v);
+						dist[v - 1] = dist[u - 1] + 1;
+					}
+					if (dist[EndNode - 1] != long.MaxValue)
+						return dist[EndNode - 1];
+				}
+			}
+			return -1;
+		}
+	}
+}
---
+++ A2/A2/Q2BipartiteGraph.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestCommon;
+
+namespace A2
+{
+	public class Q2BipartiteGraph : Processor
+	{
+		public Q2BipartiteGraph(string testDataName) : base(testDataName)
+		{}
+
+		public override string Process(string inStr) =>
+			TestTools.Process(inStr, (Func<long, long[][], long>)Solve);
+
+		public long Solve(long NodeCount, long[][] edges)
+		{			
+			//saving graph in adjacency list
+			List<long>[] adjList = new List<long>[NodeCount];
+			for (int i = 0; i < NodeCount; i++)
+				adjList[i] = new List<long>();
+			for (int i = 0; i < edges.Length; i++)
+			{
+				adjList[edges[i][0] - 1].Add(edges[i][1]);
+				adjList[edges[i][1] - 1].Add(edges[i][0]);
+			}
+
+			int[] NodesColor = new int[NodeCount];
+			//no color (not seen yet) -> 0
+			//other values -> 1, -1
+			
+			for(int i = 0; i < NodeCount; i++)
+			{
+				//if not visited
+				if (NodesColor[i] == 0)
+				{
+					long StartNode = i + 1;
+					NodesColor[StartNode - 1] = 1;
+
+					Queue<long> q = new Queue<long>();
+					q.Enqueue(StartNode);
+					while (q.Count != 0)
+					{
+						long u = q.Dequeue();
+						foreach (var v in adjList[u - 1])
+						{
+							if (NodesColor[v - 1] == 0)
+							{
+								if (NodesColor[u - 1] == 1)
+									NodesColor[v - 1] = -1;
+								else NodesColor[v - 1] = 1;
+								q.Enqueue(v);
+							}
+
+							else if (NodesColor[u - 1] == NodesColor[v - 1])
+								return 0;
+						}
+					}
+				}
+			}
+
+			return 1;
+		}
+
+	}
+}

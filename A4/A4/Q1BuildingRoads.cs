74dcae9a47f1f707abe86350c555a4c4f9f6279b
ghzamani <ghzamaninejad@gmail.com>
2020-03-14 13:15:20 +0330

    check 2 & 3 takes how much time

---
+++ A4/A4/Q1BuildingRoads.cs
@@ -0,0 +1,73 @@
+﻿using Priority_Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A4
+{
+	public class Q1BuildingRoads : Processor
+	{
+		public Q1BuildingRoads(string testDataName) : base(testDataName)
+		{}
+
+		public override string Process(string inStr) =>
+			TestTools.Process(inStr, (Func<long, long[][], double>)Solve);
+
+		public double Solve(long pointCount, long[][] points)
+		{
+			//dense graph of cities
+			List<Tuple<long, double>>[] edges = new List<Tuple<long, double>>[pointCount];
+			//first item -> neigh node
+			//second item -> dist
+
+			for (int i = 0; i < pointCount; i++)
+				edges[i] = new List<Tuple<long, double>>((int)pointCount - 1);
+						
+			for (int i = 0; i < pointCount -1; i++)
+			{
+				for (int j = i+1; j < pointCount; j++)
+				{
+					double d = Dist(points[i], points[j]);
+					edges[i].Add(new Tuple<long, double>(j, d));
+					edges[j].Add(new Tuple<long, double>(i, d));
+				}
+			}
+
+			//implementing Prim
+			double[] cost = new double[pointCount];
+			for (int i = 0; i < pointCount; i++)
+				cost[i] = int.MaxValue;
+			
+			cost[0] = 0;
+
+			SimplePriorityQueue<long, double> Q = new SimplePriorityQueue<long, double>();
+			for (int i = 0; i < pointCount; i++)
+				Q.Enqueue(i, cost[i]);			
+
+			while (Q.Count != 0)
+			{
+				long v = Q.Dequeue();
+				foreach (var z in edges[v])
+				{
+					if (Q.Contains(z.Item1) && cost[z.Item1] > z.Item2)
+					{
+						cost[z.Item1] = z.Item2;
+						Q.UpdatePriority(z.Item1, cost[z.Item1]);
+					}
+				}
+			}
+
+			double result = 0;
+			foreach (var c in cost)
+				result += c;
+			
+			return Math.Round(result, 6);
+		}
+
+		public static double Dist(long[] u, long[] v) =>
+			Math.Sqrt(Math.Pow(u[0] - v[0], 2) + Math.Pow(u[1] - v[1], 2));
+	}
+}
---
+++ A4/A4/Q2Clustering.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+using static A4.Q1BuildingRoads;
+
+namespace A4
+{
+    public class Q2Clustering : Processor
+    {
+        public Q2Clustering(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long, double>)Solve);
+
+		long[] parent;
+		long[] rank;
+		public double Solve(long pointCount, long[][] points, long clusterCount)
+        {
+			long count = 0;
+
+			double[][] edges = new double[pointCount * (pointCount - 1) / 2][];			
+
+			long k = 0;
+			for (int i = 0; i < pointCount - 1; i++)
+			{
+				for (int j = i + 1; j < pointCount; j++)
+				{
+					edges[k] = new double[] { i, j, Dist(points[i], points[j]) };
+					k++;
+				}
+			}
+
+			parent = new long[pointCount];
+			rank = new long[pointCount];
+
+			for (int i = 0; i < pointCount; i++)
+				parent[i] = i; //make set
+
+			edges = edges.OrderBy(x => x[2]).ToArray();
+
+			foreach (var edge in edges)
+			{
+				long u = (long)edge[0];
+				long v = (long)edge[1];
+				if (Find(u) != Find(v))
+				{
+					count++;
+					if (pointCount - (clusterCount - 1) == count)
+						return Math.Round(edge[2], 6);
+					Union(u, v);
+				}
+			}
+
+			return 0;
+		}
+
+		public long Find (long i)
+		{
+			while (i != parent[i])
+				i = parent[i];
+			return i;
+			//if (i != parent[i])
+			//	parent[i] = Find(parent[i]);
+			//return parent[i];
+		}
+
+		public void Union (long i,long j)
+		{
+			long iId = Find(i);
+			long jID = Find(j);
+
+			if (iId == jID)
+				return;
+
+			if (rank[iId] > rank[jID])
+				parent[jID] = iId;
+			else
+			{
+				parent[iId] = jID;
+				if (rank[iId] == rank[jID])
+					rank[jID]++;
+			}
+		}
+    }
+}

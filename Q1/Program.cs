9fe45f227aacbe9c50db88284838d08683aa8a78
ghzamani <ghzamaninejad@gmail.com>
2020-04-06 13:15:40 +0430

    hamming distance question (TA)

---
+++ Q1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string a = Console.ReadLine();
+			string b = Console.ReadLine();
+			Console.WriteLine(HammingDist(a,b));
+		}
+
+		public static string HammingDist (string A, string B)
+		{
+			string result = string.Empty;
+			int dist = 0;
+
+			for (int i = 0; i < A.Length; i++)
+			{
+				if (A[i] != B[i])
+				{
+					if (dist % 2 != 0)
+					{
+						result += A[i];
+					}
+					else
+					{
+						result += B[i];						
+					}
+					dist++;
+
+				}
+				else result += A[i];
+			}
+
+			return dist % 2 == 0 ? result : "Not Possible";
+		}
+	}
+}

aadb46ad4e15b84d0694af066667061855c7b98d
ghzamani <ghzamaninejad@gmail.com>
2020-04-13 17:00:41 +0430

    second part of Boyer-Moore added

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -10,18 +10,11 @@ namespace Q2
 			string text = Console.ReadLine();
 			string pattern = Console.ReadLine();
 
-			List<int> result = new List<int>();
 			Dictionary<char, int> table = MakeArray(pattern);
 
-			//int i = pattern.Length - 1;
-			//int j = pattern.Length - 1;
-			//while (true)
-			//{
-			//	if(text[j] == pattern[i])
-			//	{
-
-			//	}
-			//}
+			int[] result = Matches(text, pattern, table);
+			for (int i = 0; i < result.Length; i++)
+				Console.Write(result[i] + " ");
 		}
 
 		public static Dictionary<char,int> MakeArray(string str)
@@ -35,6 +28,47 @@ namespace Q2
 			}
 			result.Add('*', str.Length);
 			return result;
+		}
+
+		public static int[] Matches (string text, string pattern, Dictionary<char, int> table)
+		{
+			if (pattern.Length > text.Length)
+				return new int[1] { -1 };
+
+			List<int> indexes = new List<int>();
+
+			int i = pattern.Length - 1; //iterates over pattern
+			int j = pattern.Length - 1; //iterates over text
+			int jcopy = j;
+
+			while (j < text.Length)
+			{
+				if (text[j] == pattern[i])
+				{
+					if (i == 0) //pattern was found
+					{
+						indexes.Add(j);												
+						j = jcopy + table[text[j]];
+						jcopy = j;
+						i = pattern.Length - 1;
+						continue;
+					}
+					j--;
+					i--;
+				}
+				else
+				{
+					if (!table.ContainsKey(text[j]))
+					{
+						j = j + pattern.Length;
+					}
+					else j = jcopy + table[text[j]];
+					i = pattern.Length - 1;
+					jcopy = j;
+				}
+			}
+
+			return indexes.Count != 0 ? indexes.ToArray() : new int[1] { -1 };
 		}
 	}
 }

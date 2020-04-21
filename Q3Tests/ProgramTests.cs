d132e5df6e5d7980cdd9830b0755e4af88202a91
ghzamani <ghzamaninejad@gmail.com>
2020-04-21 19:44:30 +0430

    TA, z-algorithm pattern matching

---
+++ Q3Tests/ProgramTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Q3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Q3.Tests
+{
+	[TestClass()]
+	public class ProgramTests
+	{
+		[TestMethod()]
+		public void OccurrencesTest()
+		{
+			string text = "aabxaabxcaabxaabxay";
+			string patern = "aabx";
+			long[] results = { 0, 4, 9, 13 };
+
+			List<long> res = Program.Search(text, patern).ToList();
+			CollectionAssert.AreEqual(results, res);
+		}
+	}
+}

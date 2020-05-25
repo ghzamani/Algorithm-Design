3903a0f070b90dd2ab5daea93af66575056171c0
ghzamani <ghzamaninejad@gmail.com>
2020-05-25 21:04:55 +0430

    q1,increased by 50

--- A10/A10.Tests/GradedTests.cs
+++ A10/A10.Tests/GradedTests.cs
@@ -13,7 +13,7 @@ namespace A10.Tests
     [TestClass()]
     public class GradedTests
     {
-        [TestMethod(), Timeout(1150)]
+        [TestMethod(), Timeout(1200)]
         public void SolveTest_Q1FrequencyAssignment()
         {
             RunTest(new Q1FrequencyAssignment("TD1"));

feeddf4f0e31e762726a00859ffe7ee106332378
ghzamani <ghzamaninejad@gmail.com>
2020-06-09 22:25:33 +0430

    q3 timeout changed to 6 sec

--- A11/A11.Tests/GradedTests.cs
+++ A11/A11.Tests/GradedTests.cs
@@ -25,7 +25,7 @@ namespace A11.Tests
             RunTest(new Q2FunParty("TD2"));
         }
 
-        [TestMethod(), Timeout(5000)]
+        [TestMethod(), Timeout(6000)]
         public void SolveTest_Q3SchoolBus()
         {
             RunTest(new Q3SchoolBus("TD3"));

11f51fd0b7cb47e7ff36c7873a8dca0d3fe6b39f
ghzamani <ghzamaninejad@gmail.com>
2020-08-03 00:11:51 +0430

    part 4, strings

---
+++ Coursera/Algorithms on Strings/week1/Q1ConstructTrie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5
+{
+    public class Q1ConstructTrie
+    {
+		//static void Main(string[] args)
+		//{
+		//	long n = long.Parse(Console.ReadLine());
+		//	string[] patterns = new string[n];
+
+		//	long i = 0;
+		//	while (i < n)
+		//	{
+		//		patterns[i] = Console.ReadLine();
+		//		i++;
+		//	}
+		//	string[] result = Solve(n, patterns);
+		//	foreach(var r in result)
+		//		Console.WriteLine(r);
+		//}
+
+		public static List<string> result;
+		public static string[] Solve(long n, string[] patterns)
+        {
+			result = new List<string>();
+			Dictionary<int, List<Tuple<char, int>>> trie = MakeTrie(n, patterns);
+					
+			return result.ToArray();
+        }
+
+		public static Dictionary<int, List<Tuple<char, int>>> MakeTrie (long n, string[] patterns)
+		{
+			Dictionary<int, List<Tuple<char, int>>> trie = new Dictionary<int, List<Tuple<char, int>>>();
+
+			int nodeCount = 1;
+			for (int i = 0; i < n; i++)
+			{
+				int key = 0;
+				for (int j = 0; j < patterns[i].Length; j++)
+				{
+					if (!trie.ContainsKey(key))
+						trie.Add(key, new List<Tuple<char, int>>());
+
+					int? idx = Contain(trie[key], patterns[i][j]);
+					if (!idx.HasValue) // if the character is not in the node's children
+					{
+						trie[key].Add(new Tuple<char, int>(patterns[i][j], nodeCount));
+						trie.Add(nodeCount, new List<Tuple<char, int>>());
+						result.Add(key + "->" + nodeCount + ":" + patterns[i][j]);
+						key = nodeCount;
+						nodeCount++;
+					}
+
+					else key = trie[key][idx.Value].Item2;
+				}
+			}
+			return trie;
+		}
+		public static int? Contain(List<Tuple<char, int>> list, char x)
+		{
+			for (int i = 0; i < list.Count; i++)
+				if (list[i].Item1 == x)
+					return i; //returns the index in children list
+			
+			return null;
+		}
+	}
+}
---
+++ Coursera/Algorithms on Strings/week4/Q1FindAllOccur.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7
+{
+    public class Q1FindAllOccur
+    {
+		//static void Main(string[] args)
+		//{
+		//	string pattern = Console.ReadLine();
+		//	string text = Console.ReadLine();
+
+		//	long[] indexes = Solve(text, pattern);
+		//	if (indexes.Length == 0)
+		//	{
+		//		Console.WriteLine();
+		//		return;
+		//	}
+
+		//	foreach (var index in indexes)
+		//		Console.Write(index + " ");
+		//}
+        protected static long[] Solve(string text, string pattern)
+        {
+			if (pattern.Length > text.Length)
+				return new long[0];
+
+			List<long> indexes = new List<long>();
+
+			string s = pattern + "$" + text;
+			long[] prefixNums = ComputePrefixFunction(s);
+			for (int i = pattern.Length + 1; i < s.Length; i++)
+			{
+				if (prefixNums[i] == pattern.Length)
+					indexes.Add(i - 2 * pattern.Length);
+			}
+
+			return indexes.Count != 0 ? indexes.ToArray() : new long[0];
+        }
+
+		public static long[] ComputePrefixFunction(string str)
+		{
+			long[] prefixNums = new long[str.Length];
+			prefixNums[0] = 0;
+			long border = 0;
+			for (int i = 1; i < str.Length; i++)
+			{
+				while(border > 0 && str[i] != str[(int)border])
+				{
+					border = prefixNums[border - 1];
+				}
+				if (str[i] == str[(int)border])
+					border++;
+				else border = 0;
+				prefixNums[i] = border;
+			}
+			return prefixNums;
+		}
+	}
+}
